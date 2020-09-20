d6c454b16706220eda5e559f06adec09be5ff4b6
Ranajit <>
2020-09-20 15:20:09 +0530

    - added anyOf logic in CriteriaExecutor

--- src/main/java/com/cranajit/business_rule_engine/App.java
+++ src/main/java/com/cranajit/business_rule_engine/App.java
@@ -1,9 +1,49 @@
 package com.cranajit.business_rule_engine;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.cranajit.business_rule_engine.engine.Engine;
+import com.cranajit.business_rule_engine.model.Action;
+import com.cranajit.business_rule_engine.model.Rule;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws JsonParseException, JsonMappingException, IOException
     {
-        System.out.println( "Hello World!" );
+    	ObjectMapper mapper = new ObjectMapper();
+    	Rule rule = mapper.readValue(new File("src/main/resources/Rule1.json"), Rule.class);
+    	Rule rule2 = mapper.readValue(new File("src/main/resources/Rule2.json"), Rule.class);
+    	Rule rule3 = mapper.readValue(new File("src/main/resources/Rule3.json"), Rule.class);
+    	Rule rule4 = mapper.readValue(new File("src/main/resources/Rule4.json"), Rule.class);
+    	Rule rule5 = mapper.readValue(new File("src/main/resources/Rule5.json"), Rule.class);
+    	Rule rule6 = mapper.readValue(new File("src/main/resources/Rule6.json"), Rule.class);
+    	Rule rule7 = mapper.readValue(new File("src/main/resources/Rule7.json"), Rule.class);
+
+    	Engine engine = new Engine();
+    	engine.addRule(rule)
+    		.addRule(rule2)
+    		.addRule(rule3)
+    		.addRule(rule4)
+    		.addRule(rule5)
+    		.addRule(rule6)
+    		.addRule(rule7);
+    	
+    	
+    	Map<String, String> query = new HashMap<String, String>();
+    	query.put("PAYMENT_TYPE", "CREATE_MEMBERSHIP");
+
+    	
+    	List<Action> res = engine.run(query);
+    	for(Action r: res) {
+    		System.out.println("Action to take: " + r.getType());
+    		System.out.println("Action params: " + r.getParams());
+    	}
     }
 }
--- src/main/java/com/cranajit/business_rule_engine/engine/CriteriaExecutor.java
+++ src/main/java/com/cranajit/business_rule_engine/engine/CriteriaExecutor.java
@@ -5,11 +5,28 @@ import java.util.List;
 import java.util.Map;
 
 import com.cranajit.business_rule_engine.model.Action;
+import com.cranajit.business_rule_engine.model.Conditions;
+import com.cranajit.business_rule_engine.model.Criteria;
 import com.cranajit.business_rule_engine.model.Rule;
+
 
 public class CriteriaExecutor {
 	public static List<Action> execute(List<Rule> rules, Map<String, String> inputMap) {
 		List<Action> res = new ArrayList<Action>();
+		for(Rule rule: rules) {
+			Conditions conditions = rule.getConditions();
+			if(conditions.getAnyOf().size()!=0) {
+				for(Criteria c : conditions.getAnyOf()) {
+					try {
+						if(c.getValue().equals(inputMap.get(c.getFact()))) {
+							res.add(rule.getAction());
+						}
+					} catch (Exception e) {
+						System.out.println(c.getFact() + " RULE DID NOT MATCHED");
+					}
+				}
+			}
+		}
 		return res;
 	}
 }
