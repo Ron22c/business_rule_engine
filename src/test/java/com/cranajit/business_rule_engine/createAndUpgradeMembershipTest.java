6064895dd93499a82b9a1862a5c9a9f1a3e32bc4
Ranajit <>
2020-09-20 16:09:17 +0530

    added PaymentTypeBookTests based on some usecases

--- src/test/java/com/cranajit/business_rule_engine/createAndUpgradeMembershipTest.java
+++ src/test/java/com/cranajit/business_rule_engine/createAndUpgradeMembershipTest.java
@@ -18,7 +18,7 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class createAndUpgradeMembershipTest {
+public class CreateAndUpgradeMembershipTest {
 	private Engine engine;
 	
 	@Test
